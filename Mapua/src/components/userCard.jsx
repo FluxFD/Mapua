038ae94b387225a49a48781066311353169cff22
Timo <timothy.garcia033@gmail.com>
2024-01-20 20:46:47 +0800

    Revert "initial commit"
    This reverts commit e753fc8b5eeb330e15312dd1bbf13cd722db10ac.

--- Mapua/src/components/userCard.jsx
+++ Mapua/src/components/userCard.jsx
@@ -5,18 +5,7 @@ const userCard = () => {
   return (
     <Card elevation={3} sx={{ display: "space-between", marginBottom: 1 }}>
       <CardContent>
-        <AccountCircleIcon sx={{ fontSize: 50 }} />
-      </CardContent>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          UserName
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          StudentNumber
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <Button variant="contained">View Profile</Button>
+
       </CardContent>
     </Card>
   );
