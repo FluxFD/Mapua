ea3d55817f7df3560cf95074fd2c6fd2480ebd0a
jameschadix-ai <64976921+Satsuu@users.noreply.github.com>
2024-03-03 01:41:42 +0800

    Add storage for student

--- Map_React/src/components/pdfViewer.jsx
+++ Map_React/src/components/pdfViewer.jsx
@@ -1,20 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Container, Card, Button, Modal } from 'react-bootstrap';
-import { Editor } from '@tinymce/tinymce-react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { ref, push } from 'firebase/database';
-import useAuth from '../services/Auth';
-import { database } from '../services/Firebase'; // Update import
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
-import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+import React, { useState, useEffect, useRef } from "react";
+import { Container, Card, Button, Modal, Form } from "react-bootstrap";
+import { Editor } from "@tinymce/tinymce-react";
+import { useParams, useNavigate } from "react-router-dom";
+import { ref, push } from "firebase/database";
+import useAuth from "../services/Auth";
+import { database, storage } from "../services/Firebase"; // Update import
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+
+import {
+  getStorage,
+  ref as storageRef,
+  getDownloadURL,
+  uploadBytes,
+} from "firebase/storage";
 
 const PdfViewer = () => {
   const { url } = useParams();
   const navigate = useNavigate();
-  const [uploadedFileName, setUploadedFileName] = useState('');
+  const [uploadedFileName, setUploadedFileName] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [editorContent, setEditorContent] = useState('');
+  const [editorContent, setEditorContent] = useState("");
   const { currentUser } = useAuth();
   const editorRef = useRef(null);
 
@@ -25,7 +32,7 @@ const PdfViewer = () => {
   }, [editorContent]);
 
   const navigateToMain = () => {
-    navigate('/main');
+    navigate("/main");
   };
 
   const handleSubmit = async () => {
@@ -35,20 +42,30 @@ const PdfViewer = () => {
         uid: currentUser.uid,
         url: url,
         uploadedDate: new Date().toISOString(),
-        editorContent: editorContent
+        editorContent: editorContent,
+        uploadedFileURL: "",
       };
-  
-      const submissionRef = ref(database, 'ReviewerSubmission');
+
+      if (uploadedFileName) {
+        const uploadedFileRef = storageRef(
+          storage,
+          `students/${uploadedFileName}`
+        );
+        const uploadedFileURL = await getDownloadURL(uploadedFileRef);
+        submissionData.uploadedFileURL = uploadedFileURL;
+      }
+
+      const submissionRef = ref(database, "ReviewerSubmission");
       push(submissionRef, submissionData);
-      console.log('Submission successful!');
+      console.log("Submission successful!");
     } catch (error) {
-      console.error('Error submitting:', error);
+      console.error("Error submitting:", error);
     } finally {
+      setUploadedFileName("");
       setShowModal(false);
-      setEditorContent('');
+      setEditorContent("");
     }
   };
-  
 
   const handleEditorChange = (content, editor) => {
     setEditorContent(content);
@@ -58,11 +75,30 @@ const PdfViewer = () => {
     setShowModal(false);
   };
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setUploadedFileName(file.name);
-      // You can also upload the file to the server or perform any other necessary actions here
+  const handleFileChange = async (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      if (file) {
+        setUploadedFileName(file.name);
+
+        try {
+          const fileRef = storageRef(storage, "students/" + file.name);
+
+          await uploadBytes(fileRef, file);
+          console.log("File uploaded successfully");
+
+          const downloadURL = await getDownloadURL(fileRef);
+          setShowModal(false);
+
+          console.log("Download URL:", downloadURL);
+        } catch (error) {
+          console.error("Error uploading file:", error);
+        }
+      } else {
+        console.error("No File selected");
+      }
+    } else {
+      console.error("No files selected");
     }
   };
 
@@ -94,19 +130,19 @@ const PdfViewer = () => {
             apiKey="vcsi3l28ns46h3a66o1neo1xe4fd41pwk436gnu11sibhflm"
             value={editorContent}
             onEditorChange={handleEditorChange}
-            onInit={(evt, editor) => editorRef.current = editor}
+            onInit={(evt, editor) => (editorRef.current = editor)}
             init={{
               toolbar:
-                'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough',
-              tinycomments_mode: 'embedded',
-              tinycomments_author: 'Author name',
+                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough",
+              tinycomments_mode: "embedded",
+              tinycomments_author: "Author name",
               mergetags_list: [
-                { value: 'First.Name', title: 'First Name' },
-                { value: 'Email', title: 'Email' },
+                { value: "First.Name", title: "First Name" },
+                { value: "Email", title: "Email" },
               ],
               ai_request: (request, respondWith) =>
                 respondWith.string(() =>
-                  Promise.reject('See docs to implement AI Assistant')
+                  Promise.reject("See docs to implement AI Assistant")
                 ),
             }}
           />
@@ -120,27 +156,19 @@ const PdfViewer = () => {
         <Modal.Header closeButton>
           <Modal.Title>Upload File</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <div className="d-flex align-items-center justify-content-center">
-            <form className="file-upload-form">
-              <label htmlFor="fileInput" className="file-upload-label">
-                <div className="file-upload-design">
-                  <CloudUploadIcon style={{ width: '100%' }} />
-                  <p>Drag and Drop</p>
-                  <span className="browse-button">Browse file</span>
-                </div>
-                <input id="fileInput" type="file" onChange={handleFileChange} />
-              </label>
-            </form>
-          </div>
+        <Modal.Body className="d-flex align-items-center">
+          <Form className="file-upload-form">
+            <Form.Group controlId="fileUpload" className="mb-3">
+              <Form.Control type="file" onChange={handleFileChange} />
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
+          <Button variant="outline-secondary" onClick={closeModal}>
             Close
           </Button>
         </Modal.Footer>
       </Modal>
-
     </Container>
   );
 };
