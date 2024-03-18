7115f635a32e97104d6b1ff59643fc0696dadb6d
jameschadix-ai <64976921+Satsuu@users.noreply.github.com>
2024-03-19 06:58:18 +0800

    Add upload video task

--- Map_React/src/pages/Professor/CourseContent/ProfessorVideo.jsx
+++ Map_React/src/pages/Professor/CourseContent/ProfessorVideo.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Form, Button, Modal, Row, Col, ProgressBar } from "react-bootstrap";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import DeleteIcon from "@mui/icons-material/Delete";
+import CloseIcon from "@mui/icons-material/Close";
 
 import { ref, push, update } from "firebase/database";
 import { database, auth } from "../../../services/Firebase";
@@ -17,6 +17,33 @@ function CreateVideoModal({ show, onHide, selectedCourse }) {
   const [fileDetails, setFileDetails] = useState({ fileName: "", file: null });
   const [uploadProgress, setUploadProgress] = useState(0);
   const fileInputRef = useRef(null);
+  const [question, setQuestion] = useState("");
+  const [choices, setChoices] = useState([]);
+  const [timestamp, setTimestamp] = useState("");
+  const [maxDuration, setMaxDuration] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  const indexToLetter = (index) => String.fromCharCode(65 + index);
+
+  const handleChoiceChange = (index, value) => {
+    const newChoices = [...choices];
+    newChoices[index] = value;
+    setChoices(newChoices);
+  };
+
+  const addChoice = () => {
+    setChoices([...choices, ""]);
+  };
+
+  const removeChoice = (index) => {
+    const newChoices = [...choices];
+    newChoices.splice(index, 1);
+    setChoices(newChoices);
+  };
+
+  const handleAnswerChange = (answer) => {
+    setSelectedAnswer(answer);
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -24,7 +51,29 @@ function CreateVideoModal({ show, onHide, selectedCourse }) {
       const fileUrl = URL.createObjectURL(file);
       setVideoSource(fileUrl);
       setFileDetails({ fileName: file.name, file: file });
+
+      const video = document.createElement("video");
+
+      video.addEventListener("loadedmetadata", () => {
+        const durationInSeconds = Math.floor(video.duration);
+
+        setMaxDuration(durationInSeconds);
+      });
+
+      video.src = fileUrl;
+
+      video.load();
     }
+  };
+
+  const formatTime = (durationInSeconds) => {
+    const hours = Math.floor(durationInSeconds / 3600);
+    const minutes = Math.floor((durationInSeconds % 3600) / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+      2,
+      "0"
+    )}:${String(seconds).padStart(2, "0")}`;
   };
 
   const handleModalHide = () => {
@@ -33,7 +82,7 @@ function CreateVideoModal({ show, onHide, selectedCourse }) {
       fileInputRef.current.value = "";
     }
     setUploadProgress(0);
-
+    setMaxDuration(0);
     onHide();
   };
 
@@ -74,22 +123,48 @@ function CreateVideoModal({ show, onHide, selectedCourse }) {
           () => {
             getDownloadURL(uploadTask.snapshot.ref)
               .then((downloadURL) => {
-                console.log("File uploaded successfully");
                 const reviewerRef = ref(
                   database,
                   `VideoActivity/${selectedCourse.uid}`
                 );
                 const newFileKey = push(reviewerRef).key;
-                update(ref(database), {
-                  [`VideoActivity/${newFileKey}`]: {
-                    Course: selectedCourse.uid,
-                    title: fileName,
-                    createdBy: createdBy,
-                    date: formattedDate,
-                    video: downloadURL,
-                  },
-                });
-                console.log("File details saved to the database");
+
+                const videoActivityUpdate = {
+                  Course: selectedCourse.uid,
+                  title: fileName,
+                  createdBy: createdBy,
+                  date: formattedDate,
+                  video: downloadURL,
+                };
+                update(
+                  ref(database, `VideoActivity/${newFileKey}`),
+                  videoActivityUpdate
+                );
+
+                const activityRef = ref(
+                  database,
+                  `VideoActivity/${newFileKey}/activities`
+                );
+                const activityKey = push(activityRef).key;
+                const activityUpdate = {
+                  question: question,
+                  choices: Object.fromEntries(
+                    choices.map((choice, index) => [
+                      indexToLetter(index),
+                      choice,
+                    ])
+                  ),
+                  answer: indexToLetter(selectedAnswer),
+                  time: timestamp,
+                };
+                update(
+                  ref(
+                    database,
+                    `VideoActivity/${newFileKey}/activities/${activityKey}`
+                  ),
+                  activityUpdate
+                );
+
                 fileInputRef.current.value = "";
                 setFileDetails({ fileName: "", file: null });
                 handleModalHide();
@@ -136,6 +211,73 @@ function CreateVideoModal({ show, onHide, selectedCourse }) {
               </Col>
             </Row>
           )}
+
+          <p>Duration of the video: {formatTime(maxDuration)}</p>
+
+          <Form.Group className="mb-3" controlId="question">
+            <Form.Control
+              type="text"
+              placeholder="Enter question"
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <Form.Label>Enter choices:</Form.Label>
+              <p className="d-flex align-items-center" onClick={addChoice}>
+                <AddCircleOutlineIcon color="primary" className="me-2" />
+                Add choices
+              </p>
+            </div>
+
+            {choices.map((choice, index) => (
+              <div key={index} className="mb-3">
+                <Row className="align-items-center">
+                  <Col xs={10}>
+                    <Form.Check
+                      className="d-flex align-items-center"
+                      type="radio"
+                      aria-label={`radio ${indexToLetter(index)}`}
+                      name="choices"
+                      id={`choice${index + 1}`}
+                      label={
+                        <Form.Control
+                          className="ms-2"
+                          type="text"
+                          placeholder={`Choice ${indexToLetter(index)}`}
+                          value={choice}
+                          onChange={(e) =>
+                            handleChoiceChange(index, e.target.value)
+                          }
+                        />
+                      }
+                      onClick={() => handleAnswerChange(index)}
+                    />
+                  </Col>
+                  <Col xs={2}>
+                    {index !== 0 && (
+                      <CloseIcon
+                        color="error"
+                        className="cursor-pointer"
+                        onClick={() => removeChoice(index)}
+                      />
+                    )}
+                  </Col>
+                </Row>
+              </div>
+            ))}
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="timestamp">
+            <Form.Control
+              type="text"
+              placeholder="Enter popup duration"
+              value={timestamp}
+              onChange={(e) => setTimestamp(e.target.value)}
+            />
+          </Form.Group>
+
           {uploadProgress > 0 && (
             <ProgressBar
               now={uploadProgress}
